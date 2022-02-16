f24ec956a6e851be7b5a76cf9ad2d7a4b34b8195
Duckie Land <98952333+duckieland@users.noreply.github.com>
2022-02-16 21:34:20 +0700

    Update MMETA.sol

--- MMETA.sol
+++ MMETA.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.11;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
